6a4ae5f668aa698bf4da954ac1fcec0ab29a110c
Wei <nestcloud@gmail.com>
2022-03-27 21:51:56 +0800

    Text chagne

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'ADD'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
